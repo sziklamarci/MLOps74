2dabd3fc88310f9184179e7aa2019018ebb8d8fe
R0KKoo <gyori.zoltan1996@gmail.com>
2024-01-18 14:47:38 +0100

    latest

--- mlops74/fastapi/main.py
+++ mlops74/fastapi/main.py
@@ -1,5 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from http import HTTPStatus
+from enum import Enum
 app = FastAPI()
+
+class ItemEnum(Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/")
+def root():
+    """ Health check."""
+    response = {
+        "message": HTTPStatus.OK.phrase,
+        "status-code": HTTPStatus.OK,
+    }
+    return response
 
 @app.get("/")
 def read_root():
@@ -7,4 +23,12 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/restric_items/{item_id}")
+def read_item(item_id: ItemEnum):
+    return {"item_id": item_id}
+
+@app.get("/query_items")
+def read_item(item_id: int = Query(..., description="The item ID")):
     return {"item_id": item_id}
